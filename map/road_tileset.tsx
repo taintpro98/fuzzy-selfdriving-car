36de993f85c887e9552472140596b48e7d1f40e7
BatmanBruceWayne <taideptrai02@gmail.com>
2020-04-04 15:48:33 +0700

    create map

---
+++ map/road_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="road_tileset" tilewidth="32" tileheight="32" tilecount="2970" columns="54">
+ <image source="road_tiles.svg" width="1758" height="1760"/>
+</tileset>
