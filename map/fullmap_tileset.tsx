26df3460fa25a693aa3366fa5100bf21f4b99ced
BatmanBruceWayne <taideptrai02@gmail.com>
2020-04-04 16:24:39 +0700

    modify map

---
+++ map/fullmap_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="fullmap_tileset" tilewidth="32" tileheight="32" tilecount="1600" columns="40">
+ <image source="fullmap.jpg" width="1300" height="1285"/>
+</tileset>
